f89207a08f7cf9eb857a2df7d405edb62f3285b2
Aaron <charlesaaron577@gmail.com>
2025-01-05 21:29:55 +0530

    ss

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/arrondavide/porfolio-web.git', 
+  base: '/porfolio-web/', // Note: it matches your repo name exactly
   plugins: [react()],
 })
